bc28127eff99959c7ce9ac715693ac2a8a38243e
Rim Fardiyev <threeacesstl@gmail.com>
2018-09-24 18:16:18 -0500

    performing tests

--- TestApps/Program.cs
+++ TestApps/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
-
-namespace TestApps
+class emp
 {
-    class Program
+    public string name;
+    public string address;
+    public void display()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        Console.WriteLine("{0} is in city {1}", name, address);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        emp obj = new emp();
+        obj.name = "Akshay";
+        obj.address = "new delhi";
+        obj.display();
+        Console.ReadLine();
     }
 }
